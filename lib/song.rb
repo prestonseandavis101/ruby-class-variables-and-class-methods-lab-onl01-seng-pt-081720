c4002064266366b731fa71f2f7263cf74a9fd06b
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-08 01:52:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,17 +26,31 @@ def self.artists
 @@artists.unique 
 end
 
-def self.genre_count
-genre_count={}
-@@genres.each do |genre|
-genre_count
-[genre] += 1 
-else
-genre_count
-[genre] = 1 
-end
-end
-genre_count
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre| 
+      if genre_count[genre]
+      genre_count[genre] += 1 
+      else
+      genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+      artist_count[artist] +=1
+      else
+      artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+
 end
 end
 
